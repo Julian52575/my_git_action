f1ae4ac99dbffe9da1debec07258aaad5f369c1d
julian <julian.bottiglione@epitech.eu>
2023-02-19 14:19:26 +0100

    [fix] useless includesé

--- lib/my/my_abs.c
+++ lib/my/my_abs.c
@@ -5,17 +5,6 @@
 ** produit scalaire
 */
 
-#include <SFML/Graphics.h>
-#include <SFML/Audio.h>
-#include <SFML/Graphics/Export.h>
-#include <SFML/Graphics/Color.h>
-#include <SFML/Graphics/Rect.h>
-#include <SFML/Graphics/Types.h>
-#include <SFML/Graphics/Transform.h>
-#include <SFML/System/Vector2.h>
-#include <stddef.h>
-#include <stdio.h>
-
 float my_abs_float(float i)
 {
     if (i < 0.00) i *= -1;
